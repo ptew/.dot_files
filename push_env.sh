9e636318d8aacfe6542ae5716c822099ae21e330
ptew <parkertew@gmail.com>
2017-04-09 22:27:06 -0400

    fix scripts

--- push_env.sh
+++ push_env.sh
@@ -1,7 +1,7 @@
 # load in the files variable from the update script
 source ~/.dot_files/update_env.sh
 
-for i in $(files); do
+for i in ${files[@]}; do
   echo "Updating: " $i
   cp -r ~/$i ~/.dot_files
 done
--- update_env.sh
+++ update_env.sh
@@ -1,10 +1,11 @@
 mkdir ~/.old_dot_files
-files=(.aliases .bashrc .git-completion.bash .i3 .tmux.conf .vimrc)
+files=(.aliases .bashrc .git-completion.bash .tmux.conf .vimrc)
 
-for i in $(files); do
+for i in ${files[@]}; do
   echo "Moving: " $i
-  mv ~/$i .old_dot_files
-  cp -r ~/.dot_files/$i ~
+  mv ~/$i ~/.old_dot_files
+  echo "Copying: " $i
+  cp  ~/.dot_files/$i ~
 done
 
 source ~/.bashrc
